0b48b060c655328b5566f8733fcd8bea192ae005
expertinjection <mr.haithamiq@gmail.com>
2025-02-25 17:04:50 -0600

    added immutability and json

--- 10-arrays/09-from-of.js
+++ 10-arrays/09-from-of.js
@@ -18,7 +18,7 @@ DOES IT MODIFY ORIGINAL ITERABLE OBJECT: No
 const arr1 = [1, 2, 3];
 const arr2 = new Array('Berlin', 'Oslo', 'Toronto');
 const arr3 = Array.of('John', 'Jane', 'Alex');
-const arr4 = Array.of(name, null, 5, 10.99);
+const arr4 = Array.of(null, 5, 10.99);
 
 console.log(arr1);
 console.log(arr2);
